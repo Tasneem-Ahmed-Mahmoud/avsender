ec0535066597ef507aa4e6fec65f31235065251f
ZinabebaidZ123 <60302323+ZinabebaidZ123@users.noreply.github.com>
2024-09-10 15:21:48 +0300

    Update head-2.blade.php

--- resources/views/frontend/inc/head-2.blade.php
+++ resources/views/frontend/inc/head-2.blade.php
@@ -5,8 +5,10 @@
     <div class="container">
         <div class="row justify-content-center align-items-center hero-content"">
    
-            <h1 class=" text-center">{{ $title }}</h1>
-            <p class="text-center ">{{ $description }}</p>
+            <h1 class=" text-center" data-aos="zoom-in" data-aos-duration="500" data-aos-delay="100" 
+            data-aos-easing="linear">{{ $title }}</h1>
+            <p data-aos="zoom-in" data-aos-duration="500" data-aos-delay="200" 
+            data-aos-easing="linear" class="text-center " >{{ $description }}</p>
 
 
         </div>

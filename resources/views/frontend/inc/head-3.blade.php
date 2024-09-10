62c9e3b2ac04a4dc9cf4def8ce07baba8344767f
ZinabebaidZ123 <60302323+ZinabebaidZ123@users.noreply.github.com>
2024-09-10 15:22:19 +0300

    Update head-3.blade.php

--- resources/views/frontend/inc/head-3.blade.php
+++ resources/views/frontend/inc/head-3.blade.php
@@ -8,9 +8,12 @@
         <div class="container">
             <div class="row">
                 <div class="row justify-content-center align-items-center hero-content">
-                    <h1 class="text-center">{{ $title }}</h1>
-                    <p class="text-center w-md-75 w-100 ">{{$description}}</p>
-                    <a href="{{ url('/about') }}" class="btn btn-work-with-us text-center">{{ __('Work With Us') }}</a>
+                    <h1 data-aos="fade-up" data-aos-duration="500" data-aos-delay="100" 
+                    data-aos-easing="linear" class="text-center">{{ $title }}</h1>
+                    <p data-aos="fade-up" data-aos-duration="500" data-aos-delay="150" 
+                    data-aos-easing="linear" class="text-center w-md-75 w-100 ">{{$description}}</p>
+                    <a data-aos="zoom-in" data-aos-duration="500" data-aos-delay="200" 
+                    data-aos-easing="linear"  href="{{ url('/about') }}" class="btn btn-work-with-us text-center">{{ __('Work With Us') }}</a>
                 </div>
             </div>
         </div>

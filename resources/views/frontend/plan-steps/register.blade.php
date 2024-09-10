4381cc71d58865d21260e70cbd345d54ccd92886
ZinabebaidZ123 <60302323+ZinabebaidZ123@users.noreply.github.com>
2024-09-10 15:52:04 +0300

    Update register.blade.php

--- resources/views/frontend/plan-steps/register.blade.php
+++ resources/views/frontend/plan-steps/register.blade.php
@@ -9,7 +9,8 @@
 
 
 
-<section class="plan">
+<section class="plan" data-aos="fade-up" data-aos-duration="600" data-aos-delay="450" 
+         data-aos-easing="linear">
     <div class="faq_hero_container_section">
         <div class="container">
             <div class="d-flex justify-content-center align-items-center">

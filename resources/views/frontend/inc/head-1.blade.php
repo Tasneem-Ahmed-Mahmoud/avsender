3197fc5de25986989c2c10b898dfffd566adf661
ZinabebaidZ123 <60302323+ZinabebaidZ123@users.noreply.github.com>
2024-09-10 15:21:16 +0300

    Update head-1.blade.php

--- resources/views/frontend/inc/head-1.blade.php
+++ resources/views/frontend/inc/head-1.blade.php
@@ -3,9 +3,12 @@
         <div class="container">
             <div class="d-flex flex-column justify-content-center align-items-between" >
                 <div class="header__content">
-                    <h1>{!! __('Home Title') !!}</h1>
-                    <p>{{ __('Home Description') }}</p>
-                    <a   href="{{ url('/about') }}" class=" btn btn-work-with-us">{{ __('Work With Us') }}</a>
+                    <h1 data-aos="fade-up" data-aos-duration="500" data-aos-delay="100" 
+                    data-aos-easing="linear">{!! __('Home Title') !!}</h1>
+                    <p data-aos="fade-up" data-aos-duration="500" data-aos-delay="150" 
+                    data-aos-easing="linear" >{{ __('Home Description') }}</p>
+                    <a data-aos="zoom-in" data-aos-duration="500" data-aos-delay="200" 
+                    data-aos-easing="linear"  href="{{ url('/about') }}" class=" btn btn-work-with-us">{{ __('Work With Us') }}</a>
                 </div>
                 <div class="header__image  ">
                     <figure class=" header__img-container  d-flex justify-content-center flex-column align-items-center ">

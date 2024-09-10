1ec16838b703400f2f591271c477ac34ea4885d1
ZinabebaidZ123 <60302323+ZinabebaidZ123@users.noreply.github.com>
2024-09-10 15:22:47 +0300

    Update head.blade.php

--- resources/views/frontend/inc/head.blade.php
+++ resources/views/frontend/inc/head.blade.php
@@ -1,12 +1,15 @@
 <!-- ################################## header ############################################### -->
 
-<header class="hero  hero-with-button  d-flex justify-content-center align-items-center" >
+<header  class="hero  hero-with-button  d-flex justify-content-center align-items-center" >
     <div class="container ">
         <div class="row justify-content-center align-items-center hero-content">
    
-            <h1 class=" text-center">{{ $title }}</h1>
-            <p class="text-center">{{ $description }}</p>
-            <a href="{{ url('/about') }}" class=" btn btn-work-with-us text-center">{{ __('Work With Us') }}</a>
+            <h1 data-aos="fade-up" data-aos-duration="500" data-aos-delay="100" 
+            data-aos-easing="linear" class=" text-center">{{ $title }}</h1>
+            <p data-aos="fade-up" data-aos-duration="500" data-aos-delay="150" 
+            data-aos-easing="linear"class="text-center">{{ $description }}</p>
+            <a data-aos="zoom-in" data-aos-duration="500" data-aos-delay="200" 
+            data-aos-easing="linear" href="{{ url('/about') }}" class=" btn btn-work-with-us text-center">{{ __('Work With Us') }}</a>
 
         </div>
     </div>

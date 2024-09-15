2187a1db09d55ad0e89f087d0a90cc479c13ed25
Tasneem-Ahmed-Mahmoud <119158422+Tasneem-Ahmed-Mahmoud@users.noreply.github.com>
2024-09-15 17:26:18 +0300

    Update pricings.blade.php
    edit plan

--- resources/views/frontend/pricings.blade.php
+++ resources/views/frontend/pricings.blade.php
@@ -1,4 +1,3 @@
-
 @php
 // Convert the Eloquent collection to an array
 $plansArray = $plans->toArray();
@@ -8,21 +7,21 @@ $Enterprise = null;
 
 // Find and remove the "Enterprise" plan
 foreach ($plansArray as $index => $plan) {
-if ($plan['is_popular']) {
-$Enterprise = array_splice($plansArray, $index, 1);
-break;
-}
+    if (isset($plan['is_popular']) && $plan['is_popular']) {
+        $Enterprise = array_splice($plansArray, $index, 1);
+        break;
+    }
 }
 
 // Ensure that the "Enterprise" plan is placed in the second position
 if ($Enterprise) {
-// Insert the "Enterprise" plan at the second position if there are other plans
-if (count($plansArray) > 0) {
-array_splice($plansArray, 1, 0, $Enterprise);
-} else {
-// If no other plans, just set "Enterprise" plan as the only item
-$plansArray = $Enterprise;
-}
+    // Insert the "Enterprise" plan at the second position if there are other plans
+    if (count($plansArray) > 0) {
+        array_splice($plansArray, 1, 0, $Enterprise);
+    } else {
+        // If no other plans, just set "Enterprise" plan as the only item
+        $plansArray = $Enterprise;
+    }
 }
 
 // Convert the reordered array back to a collection
@@ -31,54 +30,48 @@ $plans = collect($plansArray);
 
 @foreach($plans as $plan)
 @php
-  
-  $plan=collect($plan);
+    // Ensure $plan is not null and is an array
+    $plan = $plan ?? [];
 @endphp
 
-<div data-aos="fade-up"  class="col-xl-4 col-md-6 col-12 mb-3 planning-card"
-  data-plan-type="{{ $plan['days'] == 30 ? 'monthly' : 'yearly' }}">
-  <div class="planing-content {{ $plan['is_popular']? 'center-plan' : 'left-plan' }}">
-    <div class="planing-top ">
-      @if ($plan['is_popular'])
-      <div class="popular-plan "> <span>{{ __('Popular')
-          }}</span> </div>
-      @endif
-      <div class="d-flex ">
+<div data-aos="fade-up" class="col-xl-4 col-md-6 col-12 mb-3 planning-card"
+     data-plan-type="{{ isset($plan['days']) && $plan['days'] == 30 ? 'monthly' : 'yearly' }}">
+    <div class="planing-content {{ isset($plan['is_popular']) && $plan['is_popular'] ? 'center-plan' : 'left-plan' }}">
+        <div class="planing-top">
+            @if (isset($plan['is_popular']) && $plan['is_popular'])
+                <div class="popular-plan"><span>{{ __('Popular') }}</span></div>
+            @endif
+            <div class="d-flex">
+                <figure class="planing-img d-flex justify-content-center align-items-center">
+                    <img src="{{ isset($plan['icon']) ? asset($plan['icon']) : '' }}" alt="">
+                </figure>
 
-
-        <figure class="planing-img  d-flex justify-content-center align-items-center">
-          <img src="{{asset( $plan['icon']) }}" alt="">
-        </figure>
-
-        <div class="planing-text ">
-          <p>{{ $plan['business_size'][app()->getLocale()]}}</p>
-          <h3>{{$plan['title'][app()->getLocale()]}}</h3>
+                <div class="planing-text">
+                    <p>{{ isset($plan['business_size'][app()->getLocale()]) ? $plan['business_size'][app()->getLocale()] : '' }}</p>
+                    <h3>{{ isset($plan['title'][app()->getLocale()]) ? $plan['title'][app()->getLocale()] : '' }}</h3>
+                </div>
+            </div>
         </div>
-      </div>
+        <div class="planing-middle">
+            <p>{{ isset($plan['description'][app()->getLocale()]) ? $plan['description'][app()->getLocale()] : '' }}</p>
+            <h6><span>{{ isset($plan['price']) ? amount_format($plan['price'], 'icon') . '/' : '' }}</span> {{ isset($plan['days']) && $plan['days'] == 30 ? __('Monthly') : __('yearly') }}</h6>
+        </div>
+        <div class="planning-bottom">
+            <h4>{{ __('What’s included') }}</h4>
+            <ul>
+                @foreach(isset($plan['limits']) ? $plan['limits'] : [] as $key => $data)
+                    <li style="display: flex">
+                        <i class="fa-solid icon {{ $data == "no" || $data == '0' ? 'bg-warning fa-xmark' : 'fa-check' }}" style="width: 26px; height: 26px"></i>
+                        <span class="{{ $data == "no" || $data == '0' ? 'text-warning' : '' }}">
+                            {{ $data == "no" || $data == "yes" ? __($key) : __($key) . " : (" . ($data == 'unlimited' ? __($data) . ')' : $data . ')') }}
+                        </span>
+                    </li>
+                @endforeach
+            </ul>
+            <a href="{{ route('choose.plan', isset($plan['id']) ? $plan['id'] : '') }}" class="btn btn-subscribe text-center w-100 white-btn">
+                {{ isset($plan['is_trial']) && $plan['is_trial'] ? __('Free ' . (isset($plan['trial_days']) ? $plan['trial_days'] : '') . ' days trial') : __('Subscribe Now') }}
+            </a>
+        </div>
     </div>
-    <div class="planing-middle">
-      <!-- <p>{{ $plan['description'][app()->getLocale()] }}</p> -->
-      <h6><span>{{ amount_format($plan['price'], 'icon') .'/'}}</span> {{ $plan['days'] == 30 ? __('Monthly') :
-        __('yearly') }}</h6>
-    </div>
-    <div class="planning-bottom">
-      <h4>{{ __('What’s included') }}</h4>
-      <ul>
-        @foreach($plan['limits'] ?? [] as $key => $data)
-        <li style="display: flex">
-          <i class="fa-solid icon {{ $data  == "no" ||$data =='0' ? 'bg-warning fa-xmark' : 'fa-check' }}"  style="width: 26px; height: 26px"></i>
-          <span class="{{$data == "no"|| $data =='0'? 'text-warning' : '' }}">
-            {{ $data=="no" ||$data=="yes"  ? __($key) : __($key) . " : (".( $data == 'unlimited' ? __($data) .')' : $data.')') }}
-
-
-          </span>
-        </li>
-        @endforeach
-      </ul>
-      <a href="{{ route('choose.plan', $plan['id']) }}" class="btn btn-subscribe text-center w-100 white-btn">
-        {{ $plan['is_trial'] ? __('Free '.$plan['trial_days'].' days trial') : __('Subscribe Now') }}
-      </a>
-    </div>
-  </div>
 </div>
 @endforeach
